3cb774a750cc433369d03448abcca70048743eb9
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-04-19 21:28:47 +0400

    Init project

---
+++ WhereIsPoliceman/WhereIsPoliceman/ViewModel/PolicemanItem.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhereIsPoliceman.ViewModel
+{
+    /// <summary>
+    /// Класс полицейского-участкового
+    /// </summary>
+    class PolicemanItem: ViewModelBase
+    {
+        public PolicemanItem()
+        {
+        }
+
+        private string _code = "";
+        /// <summary>
+        /// Код полицейского
+        /// </summary>
+        public string Code
+        {
+            get
+            {
+                return _code;
+            }
+            set
+            {
+                _code = value;
+                RaisePropertyChanged("Сode");
+            }
+        }
+    }
+}
