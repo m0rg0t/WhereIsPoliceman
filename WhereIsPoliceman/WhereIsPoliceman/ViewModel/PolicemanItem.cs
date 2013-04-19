f6eecbd98abe85d754f479a7c88fc1e3b80531b6
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-04-19 21:46:57 +0400

    PolicemanItem class

--- WhereIsPoliceman/WhereIsPoliceman/ViewModel/PolicemanItem.cs
+++ WhereIsPoliceman/WhereIsPoliceman/ViewModel/PolicemanItem.cs
@@ -31,5 +31,144 @@ namespace WhereIsPoliceman.ViewModel
                 RaisePropertyChanged("Сode");
             }
         }
+
+        private string _id = "";
+        /// <summary>
+        /// Идентификатор полицейского
+        /// </summary>
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+                RaisePropertyChanged("Id");
+            }
+        }
+
+        private string _img = "";
+        /// <summary>
+        /// Изображение искомого полицейского
+        /// </summary>
+        public string Img
+        {
+            get
+            {
+                return _img;
+            }
+            set
+            {
+                _img = value;
+                RaisePropertyChanged("Img");
+            }
+        }
+
+        private string _level1 = "";
+        /// <summary>
+        /// Первый уровень административного деления - область
+        /// </summary>
+        public string Level1
+        {
+            get
+            {
+                return _level1;
+            }
+            set
+            {
+                _level1 = value;
+                RaisePropertyChanged("Level1");
+            }
+        }
+
+        private string _level2 = "";
+        /// <summary>
+        /// Второй уровень административного деления - район
+        /// </summary>
+        public string Level2
+        {
+            get
+            {
+                return _level2;
+            }
+            set
+            {
+                _level2 = value;
+                RaisePropertyChanged("Level2");
+            }
+        }
+
+        private string _level3 = "";
+        /// <summary>
+        /// Второй уровень административного деления - населенный пункт\иное
+        /// </summary>
+        public string Level2
+        {
+            get
+            {
+                return _level3;
+            }
+            set
+            {
+                _level3 = value;
+                RaisePropertyChanged("Level3");
+            }
+        }
+
+        private string _url = "";
+        /// <summary>
+        /// Ссылка на профиль участкового
+        /// </summary>
+        public string Url
+        {
+            get
+            {
+                return _url;
+            }
+            set
+            {
+                _url = value;
+                RaisePropertyChanged("Url");
+            }
+        }
+
+        private string _fullname = "";
+        /// <summary>
+        /// Полное имя полицейского
+        /// </summary>
+        public string Fullname
+        {
+            get
+            {
+                return _fullname;
+            }
+            set
+            {
+                _fullname = value;
+                RaisePropertyChanged("Fullname");
+            }
+        }
+
+
+        private string _position = "";
+        /// <summary>
+        /// Должность полицейского
+        /// </summary>
+        public string Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                _position = value;
+                RaisePropertyChanged("Position");
+            }
+        }
+
+        
     }
 }
